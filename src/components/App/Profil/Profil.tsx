de2f2981dd00a81140272741d2c9e4bf6cc4a33d
Jonathan FLAMME <joflamme@hotmail.com>
2023-07-10 16:30:54 +0200

    div info + button editprofil/signout css

--- src/components/App/Profil/Profil.tsx
+++ src/components/App/Profil/Profil.tsx
@@ -1,15 +1,50 @@
+import { Mail, User, Key } from 'react-feather';
+
 function Profil() {
   return (
     <div className="container">
-      <div className="Profil flex justify-center	">
-        <button type="button" className="mt-10 btn rounded-3xl text-black w-28">
+      <div className="mt-10 flex justify-center mb-16	">
+        <button
+          type="button"
+          className="btn rounded-3xl text-black w-28 shadow-lg"
+        >
           Favorites
         </button>
         <button
           type="button"
-          className="mt-10 ml-10 btn rounded-3xl text-black w-28"
+          className="ml-10 btn rounded-3xl text-black w-28 shadow-lg"
         >
           Profil
+        </button>
+      </div>
+
+      <div className="flex bg-bgff border rounded-2xl mx-8 p-8">
+        <ul>
+          <li className="flex mb-8">
+            <User className="mr-4" /> Name : (utilisateur)
+          </li>
+          <li className="flex mb-8">
+            <Mail className="mr-4" />
+            Mail : (mail utilisateur)
+          </li>
+          <li className="flex mb-8">
+            <Key className="mr-4" />
+            Password : ........
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col mt-10 items-center">
+        <button
+          type="button"
+          className="btn text-sm rounded-3xl text-black w-32 mb-5 shadow-lg"
+        >
+          edit profil
+        </button>
+        <button
+          type="button"
+          className="btn rounded-3xl text-black w-32 h-1 shadow-lg"
+        >
+          Sign out
         </button>
       </div>
     </div>
