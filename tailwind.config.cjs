6ceeb94cb3beeab7fc5066874dd3b68ac487a912
Romain Vicidomini <romain.vicidomini@gmail.com>
2023-07-10 14:06:19 +0200

    tailwind - config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      'bg': '#f8f6eb',
+      'primary': '#D3DFAF',
+      'secondary': '#FFE177',
+      'third': '#1F7551',
+      'fourth':'9DC2B7'
+    }
+  },
   plugins: [require('daisyui')],
 };
