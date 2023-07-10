21cfc940903d37d13696ae0fcea32659e92af837
Jonathan FLAMME <joflamme@hotmail.com>
2023-07-10 11:47:09 +0200

    init tailwind

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="app" />;
+  return (
+    <div className="App">
+      <button className="btn text-green-600">Hello daisyUI</button>
+    </div>
+  );
 }
 
 export default App;
