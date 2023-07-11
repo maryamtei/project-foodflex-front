bff5b6f07a9baea8aedfa146e09be1c8a592548a
Jonathan FLAMME <joflamme@hotmail.com>
2023-07-11 10:40:45 +0200

    router Main.tsx + outlet App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <button type="submit" className="btn text-secondary">
         Debut du Projet
       </button>
+      <Outlet />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
 import App from './components/App/App';
 
 import './styles/index.scss';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
