574d88b34ec8b3c844612e55cc671d2b55a6bb97
Romain Vicidomini <romain.vicidomini@gmail.com>
2023-07-13 12:00:35 +0200

    ajout du scroll vers la modal lors du clic

--- src/components/settings/Modal.tsx
+++ src/components/settings/Modal.tsx
@@ -1,8 +1,11 @@
+import { scroller } from 'react-scroll';
+import { useEffect } from 'react';
 import Signup from './sign-up/Signup';
 import Signin from './sign-in/Signin';
 import { useAppSelector } from '../../hooks/redux';
 
 function Modal() {
+  const modalIsOpen = useAppSelector((state) => state.settings.modalIsOpen);
   const signUpOpen = useAppSelector((state) => state.settings.signUpOpen);
   const modalCondition = () => {
     if (!signUpOpen) {
@@ -11,7 +14,18 @@ function Modal() {
     return <Signup />;
   };
 
-  return <div className="grid justify-items-center">{modalCondition()}</div>;
+  useEffect(() => {
+    scroller.scrollTo('modalSignUpSignIn', {
+      duration: 500,
+      smooth: true,
+    });
+  }, [modalIsOpen]);
+
+  return (
+    <div id="modalSignUpSignIn" className="grid justify-items-center">
+      {modalCondition()}
+    </div>
+  );
 }
 
 export default Modal;
