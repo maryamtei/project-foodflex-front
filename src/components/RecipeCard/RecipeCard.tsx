1d8af60ed84ce7e506a5ca607491e56d7c60149c
Mathilde Louradour <mathilde.louradour@gmail.com>
2023-07-14 11:22:23 +0200

    change a to Link / add link around recipeCard

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,4 +1,5 @@
 import { Plus, Heart } from 'react-feather';
+import { Link } from 'react-router-dom';
 
 interface CardProps {
   name: string;
@@ -7,22 +8,25 @@ interface CardProps {
 
 function RecipeCard({ name, imageUrl }: CardProps) {
   return (
-    <div className="shadow-md rounded-lg relative hover:shadow-lg transition-all">
+    <Link
+      to="/recipe"
+      className="shadow-md rounded-lg relative hover:shadow-lg transition-all"
+    >
       <img src={imageUrl} alt={name} className="rounded-t-md cover" />
       <div className="text-bgff absolute top-2 right-1">
         <div className="card-actions justify-end">
-          <a href="./">
+          <Link to="/">
             <Heart />
-          </a>
-          <a href="./">
+          </Link>
+          <Link to="/">
             <Plus />
-          </a>
+          </Link>
         </div>
       </div>
       <div className="rounded-b-lg">
         <h2 className="text-thirdff p-2 text-center truncate">{name}</h2>
       </div>
-    </div>
+    </Link>
   );
 }
 
