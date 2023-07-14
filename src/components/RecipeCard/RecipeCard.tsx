765841050a5c812bb398ee479c7dafc9b2eb3b6c
Mathilde Louradour <mathilde.louradour@gmail.com>
2023-07-14 11:55:20 +0200

    add opacity around cards icons

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -13,18 +13,26 @@ function RecipeCard({ name, imageUrl }: CardProps) {
       className="shadow-md rounded-lg relative hover:shadow-lg transition-all"
     >
       <img src={imageUrl} alt={name} className="rounded-t-md cover" />
-      <div className="text-bgff absolute top-2 right-1">
-        <div className="card-actions justify-end">
-          <Link to="/">
-            <Heart />
+      <div className="text-bgff absolute top-2 right-1 ">
+        <div className="card-actions justify-end bg-t">
+          <Link
+            to="/"
+            className="hover:text-secondaryff transition-all bg-gray-700/50 rounded-full p-2"
+          >
+            <Heart size={20} />
           </Link>
-          <Link to="/">
-            <Plus />
+          <Link
+            to="/"
+            className="hover:text-secondaryff transition-all bg-gray-700/50 rounded-full p-2"
+          >
+            <Plus size={20} />
           </Link>
         </div>
       </div>
       <div className="rounded-b-lg">
-        <h2 className="text-thirdff p-2 text-center truncate">{name}</h2>
+        <h2 className="text-fourthff font-bold p-2 text-center truncate">
+          {name}
+        </h2>
       </div>
     </Link>
   );
