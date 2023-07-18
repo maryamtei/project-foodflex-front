ef2b387fae7af1563ffe2edbc9ca76b769e17578
Mathilde Louradour <mathilde.louradour@gmail.com>
2023-07-18 18:03:00 +0200

    css adjustment

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -23,9 +23,9 @@ function Recipe() {
     return <p>Loading...</p>;
   }
   return (
-    <div className="container mx-auto  ">
+    <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold m-8 text-red">{recipe.name}</h1>
+        <h1 className="text-4xl font-bold m-8">{recipe.name}</h1>
         <img
           src={recipe.imageUrl}
           alt={recipe.name}
@@ -36,21 +36,21 @@ function Recipe() {
         Meal Preparation
       </h2>
       <div className="flex flex-col md:flex-row p-4">
-        <div className="md:hidden  md:w-1/3 md:pl-8 justify-center text-center foodPattern bg-opacity-25 p-2 rounded-xl shadow-md border border-fourthff text-bgff">
+        <div className="md:hidden md:w-1/3 md:pl-8">
           <IngredientsList
             ingredients={recipe.ingredients}
             mesures={recipe.mesures}
           />
         </div>
-        <div className="md:w-2/3 md:pr-8 pt-4 md:p-0 ">
+        <div className="md:w-2/3 md:pr-8 pt-4 md:p-0">
           <h2 className="text-xl font-bold mb-2">Instructions:</h2>
-          <div className="prose  lg:prose-lg">
+          <div className="prose lg:prose-lg">
             {recipe.instruction.split('\n').map((line) => (
               <p key={line}>{line}</p>
             ))}
           </div>
         </div>
-        <div className="hidden md:block md:w-1/3  justify-center text-center foodPattern bg-opacity-25 p-4 rounded-xl shadow-md border border-fourthff text-bgff">
+        <div className="hidden md:block md:w-1/3  ">
           <IngredientsList
             ingredients={recipe.ingredients}
             mesures={recipe.mesures}
