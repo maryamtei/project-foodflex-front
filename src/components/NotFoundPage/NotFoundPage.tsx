43fedc66f1f0087bb40fb77ccaab1c8d7cedd6e7
Gabrielle Pgd <gpagnard@gmail.com>
2023-07-19 15:52:46 +0200

    Page 404

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,18 +1,24 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage() {
   return (
     <div className="relative translate-y-[-64px]">
       <img
-        src="/ingredients.jpg"
+        src="./img/404.jpg"
         alt="Background"
-        className="w-full h-screen sm:h-auto sm:max-h-128 object-cover object-bottom "
+        className="w-full h-screen sm:h-auto sm:max-h-128 object-cover object-bottom"
         style={{
           objectPosition: 'center top',
         }}
       />
-      <div className="absolute inset-0 bg-black opacity-50" />
-      <h1 className="w-auto text-2xl sm:text-3xl md:text-5xl  font-bold text-white text-center sm:text-center md:text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 uppercase">
-        Culinary creativity unleashed with Food-flex!
-      </h1>
+      <div className="absolute inset-x-0 bottom-20 flex justify-center">
+        <Link
+          to="/"
+          className="px-8 py-3 text-lg font-bold text-red-700 bg-fourthff rounded hover:bg-thirdff"
+        >
+          HOME
+        </Link>
+      </div>
     </div>
   );
 }
