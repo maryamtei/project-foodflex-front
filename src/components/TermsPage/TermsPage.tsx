ad32f7773fd7cc1b7f12d7d61545e1897bf48661
Gabrielle Pgd <gpagnard@gmail.com>
2023-07-19 16:42:24 +0200

    Terms and Conditions of Use Page

---
+++ src/components/TermsPage/TermsPage.tsx
@@ -0,0 +1,50 @@
+function TermsPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">Terms and Conditions of Use</h1>
+
+      <div className="prose">
+        <h2>Introduction</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed
+          nunc at tellus congue condimentum. Quisque ac est neque. Donec
+          elementum arcu id tortor convallis feugiat. In in varius elit. Sed
+          vitae ipsum fringilla, condimentum libero et, aliquet nulla. Donec
+          bibendum semper ex, ut ultricies lorem lobortis ut.
+        </p>
+
+        <h2>Acceptance of Terms</h2>
+        <p>
+          By accessing or using our website, you agree to be bound by these
+          Terms and Conditions of Use. If you do not agree with any part of
+          these terms, then you may not access or use our website.
+        </p>
+
+        <h2>Intellectual Property</h2>
+        <p>
+          All content included on our website, such as text, graphics, logos,
+          images, and software, is the property of our company and is protected
+          by copyright and other applicable laws.
+        </p>
+
+        <h2>Disclaimer</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed
+          nunc at tellus congue condimentum. Quisque ac est neque. Donec
+          elementum arcu id tortor convallis feugiat. In in varius elit. Sed
+          vitae ipsum fringilla, condimentum libero et, aliquet nulla. Donec
+          bibendum semper ex, ut ultricies lorem lobortis ut.
+        </p>
+
+        <h2>Governing Law</h2>
+        <p>
+          These Terms and Conditions of Use shall be governed by and construed
+          in accordance with the laws of [Your Country/State], without regard to
+          its conflict of law provisions.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TermsPage;
