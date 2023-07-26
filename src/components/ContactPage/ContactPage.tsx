911bb10b71db1357f6603cc765f36453f3e2718a
Gabrielle Pgd <gpagnard@gmail.com>
2023-07-26 15:46:20 +0200

    page contact finalisée

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -18,108 +18,113 @@ function ContactPage() {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/submit-contact-form', {
+      console.log( JSON.stringify(formData))
+      const response = await fetch('http://localhost:3000/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
-
+  
       if (response.ok) {
         const data = await response.json();
         console.log('Message successfully sent!', data);
-        // Ajoutez ici le code pour afficher un message de succès ou rediriger l'utilisateur vers une autre page
+  
+        setFormData({ name: '', email: '', message: '' });
       } else {
         const errorData = await response.json();
         console.error('Error sending form', errorData);
-        // Ajoutez ici le code pour afficher un message d'erreur à l'utilisateur
       }
     } catch (error) {
       console.error('Error submitting form', error);
-      // Ajoutez ici le code pour gérer les erreurs d'envoi (par exemple, afficher un message d'erreur générique)
     }
   };
 
   return (
     <div>
-      <div className="relative w-full">
+      <div className="relative w-full h-screen">
+        {/* Apply overlay and blur effect to the image */}
         <img
           src="./img/contact.jpg"
           alt="Background"
-          className="w-full h-full object-cover object-bottom"
+          className="w-full h-full object-cover object-bottom absolute inset-0"
           style={{
             objectPosition: 'center top',
+            zIndex: -1,
+            filter: 'blur(4px)',
           }}
         />
-        <div className="absolute inset-0 bg-black opacity-50" />
-        <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-white text-center sm:text-center md:text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 uppercase">
-          Contact
-        </h1>
-      </div>
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="bg-thirdff w-full max-w-md p-8 rounded shadow">
-          <h2 className="text-red-500 text-4xl text-center font-bold mb-6">
-            Contact us
-          </h2>
-          <form>
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-red-500 font-medium mb-2"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full px-3 py-2 border border-red-200 rounded focus:outline-none focus:border-red-700"
-                placeholder="Please enter your full name"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-red-500 font-medium mb-2"
-              >
-                E-mail address
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full px-3 py-2 border border-red-200 rounded focus:outline-none focus:border-red-700"
-                placeholder="Please enter your e-mail address"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="message"
-                className="block text-red-500 font-medium mb-2"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                rows={4}
-                className="w-full px-3 py-2 border border-red-200 rounded focus:outline-none focus:border-red-700"
-                placeholder="Please enter your message"
-                required
-              />
-            </div>
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="bg-fourthff hover:bg-red-500 text-white font-medium py-2 px-4 rounded focus:outline-none"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+        <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-thirdff w-full max-w-md p-8 rounded shadow-md border border-gray-700"> {/* Add shadow */}
+            <h1 className="text-red-500 text-4xl text-center font-bold mb-6">
+              Contact us
+            </h1>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-red-500 font-medium mb-2 "
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-500 rounded focus:outline-none focus:border-black text-gray-600 bg-bgff"
+                  placeholder="Please enter your full name"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-red-500 font-medium mb-2"
+                >
+                  E-mail address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-500 rounded focus:outline-none focus:border-black text-gray-600 bg-bgff"
+                  placeholder="Please enter your e-mail address"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="block text-red-500 font-medium mb-2"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  rows={4}
+                  className="w-full px-3 py-2 border border-gray-500 rounded focus:outline-none focus:border-black text-gray-600 bg-bgff"
+                  placeholder="Please enter your message"
+                  required
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="bg-fourthff hover:bg-red-500 text-white font-medium py-2 px-4 rounded focus:outline-none"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
