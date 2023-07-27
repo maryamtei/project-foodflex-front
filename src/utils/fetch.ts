c4a51afc08994de0403ed4a32c7333898435b37b
Jonathan FLAMME <joflamme@hotmail.com>
2023-07-27 09:26:05 +0200

    config Netlify-Heroku

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://foodflex-oclock-46f540112ea5.herokuapp.com';
 
 // Fonction utilitaire pour ajouter le token d'authentification aux headers
 const createHeadersWithAuthorization = () => {
