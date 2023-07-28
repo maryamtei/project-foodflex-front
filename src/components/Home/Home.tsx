cbac5be43f7fd2c2b7d27ac0a3ab9bf66329af68
Mathilde Louradour <mathilde.louradour@gmail.com>
2023-07-28 11:00:50 +0200

    add modal on Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,6 +6,7 @@ import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { changeStateHome } from '../../store/reducers/home';
 import { fetchRandomRecipes } from '../../store/reducers/recipes';
 import { toggleIsOpen, toggleSignUpOpen } from '../../store/reducers/settings';
+import ScheduleModal from '../ScheduleModal/ScheduleModal';
 
 interface HomeProps {
   signInDomain: string;
@@ -78,6 +79,8 @@ function Home({ signInDomain }: HomeProps) {
         modalIsOpen ? 'sm:blur-[3px] sm:pointer-events-none' : ''
       } `}
     >
+      <ScheduleModal />
+
       <HeadHome />
       {recipes.length === 14 && <MainHome recipes={recipes} />}
 
