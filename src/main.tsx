c0095db5cd851770865711fb53c1d4c1c0b95db1
Romain Vicidomini <romain.vicidomini@gmail.com>
2023-07-29 13:11:49 +0200

    commentaire fetch.ts

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import './styles/index.scss';
 import App from './components/App/App';
 import Favoris from './components/Profil/Favoris';
 import Home from './components/Home/Home';
-import Apptest from './components/App/Apptest';
 import Recipes from './components/Recipes/Recipes';
 import Recipe from './components/Recipe/Recipe';
 import Schedule from './components/Schedule/Schedule';
@@ -24,22 +23,18 @@ import AboutUsPage from './components/AboutUsPage/AboutUsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Apptest />}>
+    <Route path="/" element={<App />}>
       <Route index path="/" element={<Home signInDomain="" />} />
       <Route path="/sign-up" element={<Home signInDomain="signup" />} />
       <Route path="/sign-in" element={<Home signInDomain="signin" />} />
       <Route path="/profil" element={<Profil />} />
       <Route path="/favoris" element={<Favoris />} />
       <Route path="/schedule" element={<Schedule />} />
-      {/* <Route path="/login" element={<Apptest />} /> */}
-      {/* Modifier les routes et l'élement Apptest en fonction de vos taches */}
-      {/* <Route index element={<Apptest />} /> */}
       <Route path="recipes" element={<Recipes />} />
       <Route path="recipes/:id" element={<Recipe />} />
       <Route path="/terms" element={<TermsPage />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path="/about" element={<AboutUsPage />} />
-      {/* <Route path="/404" element={<NotFoundPage />} /> */}
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
